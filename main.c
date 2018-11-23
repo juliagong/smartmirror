a41f89962d141ae24dd54b8e1ce39b216244d7ea
Evander Deocariza <evandeo@stanford.edu>
2018-11-22 23:24:03 -0800

    Temp sensor works.

--- main.c
+++ main.c
@@ -24,102 +24,71 @@ void check_for_motion(void)
     }
     }
 }
-
-//////////////// Temp & Humidity Sensor ////////////////
-#define MAXTIMINGS  85
-#define TEMP_PIN    GPIO_PIN26
-int temp_data[5] = { 0 };
-
-/*
-* In order to request that DHT11 sends data, have to set the pin to output and write low then high. 
-*/
-void request_temp_data(void) {
-    int ack = 0;
-    int attempt = 0; 
-    
-
-    while (!ack) {
-        counter++; 
-        printf("Requesting temperature data... (Attempt %d) \n", attempt);
-        gpio_set_output(TEMP_PIN);
-        gpio_write(TEMP_PIN, 0);
-        timer_delay_ms(25);         // Data sheet says wait at least 18ms
-        gpio_write(TEMP_PIN, 1); 
-        timer_delay_us(20);         // Pull-up for 20us
-
-        // Prepare to receive ack
-        gpio_set_input(TEMP_PIN);    
-
-        // Wait 40us for ack. If no ack, send another request 
-        int counter = 0;
-        while ( !ack && counter < 40 ) { // Wait for sensor to pull down the signal
-            if (gpio_read(TEMP_PIN) == 0) ack = 1; 
-            timer_delay_us( 1 );
-            counter++;
-        }
-    }
-}
+ 
+//////////////// Temperature & Humidity  Sensor ////////////////
+#define MAX_PIN_FLIPS 85       // Max number of times the pin switches between 0 or 1
+#define TIMEOUT_COUNT 255      // Used to exit loop if timeout occurs while waiting for data 
+#define TEMP_PIN  GPIO_PIN26
 
 /*
-A complete data transmission is 40bit, and the sensor sends higher data bit first.
-Data format: 8bit integral RH data + 8bit decimal RH data + 8bit integral T data + 8bit decimal T data + 8bit check sum. 
-Check Sum: "8bit integral RH data + 8bit decimal RH data + 8bit integral T data + 8bit decimal T data"
-
+ This function uses TEMP_PIN set to output to request data from the DHT11 temperature and 
+ humidity sensor. The function then sets TEMP_PIN to input and monitors it for flips between
+ 0 and 1. Flips 0 - 3 are part of ACK sequence after requesting data from the sensor.
+ Flip 4 is the first data bit. Even-numbered flips are data bits while odd-numbered flips 
+ are the pauses (low) that precede each data bit. 
 */
 void read_temp_data() {
-    
-    int last_state = 1;        // Last state high
     unsigned int counter = 0;
-    unsigned int j = 0; 
-    float   f; 
+    unsigned int n_bits = 0; 
+    int temp_data[5] = { 0 };
 
-    request_temp_data();
+    // Request data from sensor
+    gpio_set_output(TEMP_PIN);
+    gpio_write(TEMP_PIN, 0);
+    timer_delay_ms(18);
+    gpio_write(TEMP_PIN, 1);
+    timer_delay_us(40);
+    gpio_set_input( TEMP_PIN);
 
-
-    printf("Receiving temperature data... \n");
-
-/* 
- This code is off the website, but I haven't gone through it carefully enough to understand if it's doing what it's supposed to. 
- Will work on this more later.  
-*/
-    // for (int i = 0; i < MAXTIMINGS; i++ ) {
-    //     counter = 0;
-    //     while ( gpio_read( TEMP_PIN ) == last_state ) { // Check every usec for update and break if no change after 255 usecs
-    //         counter++;
-    //         timer_delay_us( 1 );
-    //         if ( counter == 255 ) break;
-    //     }
-    //     last_state = gpio_read(TEMP_PIN);
+    int last_state = 1;        // rpi sets pin 1 then waits for DHT to set it 0
  
-    //     if ( counter == 255 ) break;
+    for (int flips = 0; flips < MAX_PIN_FLIPS; flips++ ) {
+        counter = 0;
+        while (gpio_read(TEMP_PIN) == last_state ) { // Spine while waiting for pin to flip 
+            counter++;
+            timer_delay_us(1);
+            if (counter == TIMEOUT_COUNT) break; 
+        }
+        last_state = gpio_read( TEMP_PIN );
+        
+        if (counter == TIMEOUT_COUNT) break;
  
-    //     if ( (i >= 4) && (i % 2 == 0) ) {
-    //         temp_data[j / 8] <<= 1;
-    //         if ( counter > 16 )
-    //             temp_data[j / 8] |= 1;
-    //         j++;
-    //     }
-    // }
- 
-    // if ( (j >= 40) &&
-    //      (temp_data[4] == ( (temp_data[0] + temp_data[1] + temp_data[2] + temp_data[3]) & 0xFF) ) )
-    // {
-    //     f = temp_data[2] * 9. / 5. + 32;
-    //     printf( "Humidity = %d.%d %% Temperature = %d.%d C (%.1f F)\n",
-    //         temp_data[0], temp_data[1], temp_data[2], temp_data[3], f );
-    // }else  {
-    //     printf( "Data not good, skip\n" );
-    // }
+        // Flips 0-3 are part of ACK. Odd-numbered bits are the pause before data bits. 
+        if ( (flips >= 4) && (flips % 2 == 0) ) {
+            temp_data[n_bits / 8] <<= 1;
+            if ( counter > 16 )
+                temp_data[n_bits / 8] |= 1;
+            n_bits++;
+        }
+    }
+    
+    unsigned int checksum = (temp_data[0] + temp_data[1] + temp_data[2] + temp_data[3]) & 0xFF; 
+    
+    if ( (n_bits >= 40) && (temp_data[4] == checksum ) ) {
+        printf( "Temperature = %d.%d C Humidity = %d.%d%% \n", temp_data[2], temp_data[3], temp_data[0], temp_data[1] );
+    } else {
+        printf( "Data not good, skip\n" );
+    }
 }
- 
 
 void test_temp_sensor(void) {
+    int iter = 10; 
     printf( "Testing DHT11 Temperature & Humidity sensor...\n" );
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < iter; i++) {
+        printf("Reading #%d/%d\n", i+1, iter); 
         read_temp_data();
-        timer_delay(1);
+        timer_delay(2);
     }
-    return(0);
 }
 
 
@@ -129,13 +98,12 @@ void main(void)
     gpio_init();
     timer_init();
 
-    // Test motion sensor
-    setup_motion_sensor();
-    check_for_motion();
+    // // Test motion sensor
+    // setup_motion_sensor();
+    // check_for_motion();
 
     // Test temperature sensor
-    // setup_temp_sensor();
-    // test_temp_sensor(); 
+    test_temp_sensor(); 
 
 }
 
