4c5e4e3f768af62df77db26eef18a3689f6af373
Julia Gong <jxgong@stanford.edu>
2018-11-28 13:33:10 -0800

    Added motion sensing blocking code.

--- main.c
+++ main.c
@@ -1,8 +1,36 @@
 #include "printf.h"
 #include "uart.h"
+#include "gpio.h"
+#include "timer.h"
 
-void main(void) 
+#define MOTION_PIN GPIO_PIN26
+
+void setup_motion_sensor(void)
 {
-  uart_init();
-  printf("Hello, world!\n");
+    gpio_set_input(MOTION_PIN);
 }
+
+void check_for_motion(void)
+{
+    timer_delay(2);
+    printf("Warmed up! Checking for motion...\n");
+    int count = 0;
+    while (1) {
+	if (gpio_read(MOTION_PIN)) {
+	    printf("Motion detected %d!\n", count + 1);
+	    timer_delay(1);
+	    count++;
+	}
+    }
+}
+
+void main(void)
+{
+    uart_init();
+    gpio_init();
+    timer_init();
+
+    setup_motion_sensor();
+    check_for_motion();
+}
+
