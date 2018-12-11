eae51bae01394d07fc8968c141449943ceee2393
Evander Deocariza <evandeo@stanford.edu>
2018-12-11 13:48:05 -0800

    Updated constants to account for extra header which signals if the array holds *TIME or *WEATHER data

--- headers/output_formatter.h
+++ headers/output_formatter.h
@@ -2,14 +2,15 @@
 #define FORMATTER_H
 
 enum datetime_data {
-    DATETIME_DOW = 0,
-    DATETIME_MONTH_NAME = 1,
-    DATETIME_MONTH = 2,
-    DATETIME_DAY = 3,
-    DATETIME_YEAR = 4,
-    DATETIME_HOUR = 5,
-    DATETIME_MINUTE = 6,
-    DATETIME_SECOND = 7
+	DATETIME_HEADER = 0,
+    DATETIME_DOW = 1,
+    DATETIME_MONTH_NAME = 2,
+    DATETIME_MONTH = 3,
+    DATETIME_DAY = 4,
+    DATETIME_YEAR = 5,
+    DATETIME_HOUR = 6,
+    DATETIME_MINUTE = 7,
+    DATETIME_SECOND = 8
 };
 
 void format_temperature_data(char* buf, int bufsize, unsigned int settingId);
