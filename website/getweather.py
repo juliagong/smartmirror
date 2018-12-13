3ff6e0c98291a986737c70509856f026d1703614
Evan Deocariza <35383635+evandeo@users.noreply.github.com>
2018-12-12 16:36:35 -0800

    Split floats into int and dec parts

--- website/getweather.py
+++ website/getweather.py
@@ -2,54 +2,60 @@
 import pyowm
 
 # Free API key
-owm = pyowm.OWM('2fc3793643d86bd1743513bbed775d24')
+owm = pyowm.OWM('REDACTED')
 
-city_id = 5380748   # City ID for Palo Alto
+# City ID for Palo Alto
+city_id = 5380748   
+observation = owm.weather_at_id(city_id)               
+weather = observation.get_weather()                    
 
+# Current weather status
+weather_status = weather.get_detailed_status().replace(" ", "-");
 
-observation = owm.weather_at_id(city_id)                # Get Observation object
-forecaster = owm.three_hours_forecast_at_id(city_id)    # Get Forecaster object
-
-weather = observation.get_weather()                     # Get Weather object
-forecast = forecaster.get_forecast()                   # Get Forecast object
-weathers = forecast.get_weathers()
-n_weathers = forecast.count_weathers()
-
-
+# Current temp in fahrenheit
 temp_curr_F =  list(weather.get_temperature('fahrenheit').values())[0]
-# temp_max_F = list(weather.get_temperature('fahrenheit').values())[1]
-# temp_min_F = list(weather.get_temperature('fahrenheit').values())[2]
+
+# Split into integer and decimal parts
+temp_F_int = str(temp_curr_F).split('.')[0]
+temp_F_dec = str(temp_curr_F).split('.')[1]
+
+# Current temp in celsius
 temp_curr_C =  list(weather.get_temperature('celsius').values())[0]
-# temp_max_C = list(weather.get_temperature('celsius').values())[1]
-# temp_min_C = list(weather.get_temperature('celsius').values())[2]
-wind_speed = list(weather.get_wind('miles_hour').values())[0]
+
+# Split into integer and decimal parts
+temp_C_int = str(temp_curr_C).split('.')[0]
+temp_C_dec = str(temp_curr_C).split('.')[1]
+
+# Other current weather data
+wind_speed = int(list(weather.get_wind('miles_hour').values())[0])
 humidity = weather.get_humidity()
 
-print "Content-type: text/html"
-# Have to print <html> tags for page to display
 
-print """
-<html><body>
-"""
-print("<br>UTC: " + str( weather.get_reference_time('date') ) )
-print("<br>Location: *" + observation.get_location().get_name() + "\n<br>Weather: *" + weather.get_detailed_status())
-print("<br>Temperature: *" + str(temp_curr_F) + " F / *" + str(temp_curr_C) + " C")
-# print("<br>Max: *" + str(temp_max_F) + " F / *" + str(temp_max_C) + " C")
-# print("<br>Min: *" + str(temp_min_F) + " F / *" + str(temp_min_C) + " C")
-print("<br>Wind: *" + str(wind_speed) + " MPH")
-print("<br>Humidity: *" + str(humidity) + "%")
-# Prints the OMW weather code. There are only 6 corresponding weather icon groups
+######## Begin printed output #######
+
+print ("Content-type: text/plain\n")
+
+print(" *" + weather_status)
+print(" *" + str(temp_F_int))
+print(" *" + str(temp_F_dec))
+print(" *" + str(temp_C_int))
+print(" *" + str(temp_C_dec))
+print(" *" + str(wind_speed) + " ")
+print(" *" + str(humidity) + "% ")
 # 2xx Thunder, 3xx&5xx rain, 6xx snow, 7xx 'atmosphere', 800 clear, 8xx clouds
-print("<br>Weather Code: *" + str(weather.get_weather_code()))
-# # Returns times in UST. Don't want to bother with conversion
-# print("\nSunset: *" + str( weather.get_sunset_time('iso') ) )
-# print("\nSunrise: *" + str( weather.get_sunrise_time('iso') ) )
-# # Rain & Snow are empty when tested... probably don't return any data unless it's actually raining or snowing
-# print(list(weather.get_rain()))
-# print(list(weather.get_snow()))
+print(" *" + str(weather.get_weather_code()) + " ")
 
+######## End printed output #######
 
 # Disable until can represent forecast coherently 
+# Currently gets weather forecast for next 5 days in 3 hour increments
+# The reference time for each weather object is also in UTC. 
+
+# # forecaster = owm.three_hours_forecast_at_id(city_id)    # Get Forecaster object
+# forecast = forecaster.get_forecast()                   # Get Forecast object
+# weathers = forecast.get_weathers()
+# n_weathers = forecast.count_weathers()
+
 # print("\n5-Day Weather Forecast (in 3-hour increments): \n")
 
 # for i in range(n_weathers):
@@ -68,8 +74,3 @@ print("<br>Weather Code: *" + str(weather.get_weather_code()))
 #     print("<br>Max: " + str(temp_max_F) + " F / " + str(temp_max_C) + " C")
 #     print("<br>Min: " + str(temp_min_F) + " F / " + str(temp_min_C) + " C")print("<br>")
 #     print("")
-
-
-print """
-</body></html>
-"""
